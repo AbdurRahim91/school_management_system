07ac33431eb47f78c1cdb1488a2690fdb92308b6
munaimpro <khanmail2599@gmail.com>
2023-09-02 23:43:06 +0600

    Static Page design complete

---
+++ Include/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Register System PHP</title>
+	<link rel="stylesheet" href="Include/bootstrap3.3.7.min.css"/>
+</head>
+<body>
+	<div class="container">
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="index.php">School Management System</a>
+				</div>
+				<ul class="nav navbar-nav pull-right">
+					<li><a href="index_munaim.php">Home</a></li>
+					<li><a href="signup_munaim.php">Register</a></li>
+					<li><a href="signin_munaim.php">Login</a></li>
+					<li><a href="">Logout</a></li>
+				</ul>
+			</div>
+		</nav>
