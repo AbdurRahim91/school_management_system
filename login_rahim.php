af00d5a0879299ade8c4e789215f7bec9cae6ba7
abdur_rahim <rahimbd91@gmail.com>
2023-09-02 23:56:46 +0600

    new update 010923

--- login_rahim.php
+++ login_rahim.php
@@ -3,9 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel = "stylesheet" type = "text/css" href = "assets/css/custom_style.css">  
+    <title>Login_page</title>
 </head>
 <body>
-Hoell How are You
+<div id = "login">  
+        <h1>Login</h1>  
+        <form action = "auth.php" method = "POST">  
+            <p>  
+                <label> UserName: </label>  
+                <input type = "text" name  = "user" />  
+            </p>  
+            <p>  
+                <label> Password: </label>  
+                <input type = "password" name  = "pass" />  
+            </p>  
+            <p>     
+                <input type =  "submit" value = "Login" />  
+            </p>  
+        </form>  
+    </div>  
 </body>
 </html>
