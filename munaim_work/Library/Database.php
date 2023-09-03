d40002243a0051cea3fa34d8e0ce88a1edfd403b
munaimpro <khanmail2599@gmail.com>
2023-09-03 16:42:13 +0600

    Database connection message removed

--- munaim_work/Library/Database.php
+++ munaim_work/Library/Database.php
@@ -4,15 +4,13 @@
         private $dbuser = "root";
         private $dbpass = "";
         private $dbname = "sms";
+		public $pdo;
 
         public function __construct(){
             if(!isset($this->pdo)){
                 try{
 					$link = new PDO("mysql:host=".$this->dbhost."; dbname=".$this->dbname,$this->dbuser,$this->dbpass);
 					$this->pdo = $link;
-					if($this->pdo==true){
-						return true;
-					}
 				} catch(PDOException $e){
 					die("Failed to connect with database...".$e->getMessage());
 				}
