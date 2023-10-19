d5f1c9c79e69936f477c02c14fc7fd82c935c1c5
abdur_rahim <rahimbd91@gmail.com>
2023-10-19 22:41:25 +0600

    notice updated on master_file

---
+++ SMS-DENO/includes/footer.php
@@ -0,0 +1,78 @@
+<!-- Footer Section Start -->
+<footer>
+    <img src="images/wave2.png" alt="wave" class="w-100">
+    <div class="container pt-5">
+        <div class="footer-top">
+            <div class="row">
+                <div class="col-md-4 footer-contact">
+                    <p class="text-uppercase fw-bold">contact us</p>
+                    <p>
+                        <i class="fas fa-location-pin"></i>
+                        World Trade Center, Bengalore
+                    </p>
+                    
+                    <p>
+                        <i class="fas fa-phone"></i>
+                        +0023435345
+                    </p>
+
+                    <p>
+                        <i class="fas fa-envelope"></i>
+                        xyz@gmail.com
+                    </p>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="footer-logo">
+                        <img class="rounded" src="images/logos/cumilla.jpg" alt="Footer Logo">
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur totam doloribus nam nostrum maxime. Maiores ad optio possimus dolorum! Sint!</p>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="footer-sitemap">
+                        <h5>OUR SITEMAP</h5>
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="about.php">About</a></li>
+                            <li><a href="admisssion.php">Admission Information</a></li>
+                            <li><a href="news.php">News &amp; Events</a></li>
+                            <li><a href="#">Why Study At NDC</a></li>
+                            <li><a href="#">Governing Body</a></li>
+                            <li><a href="#">Alumni</a></li>
+                            <li><a href="#">Faculty &amp; Staff</a></li>
+                            <li><a href="#">Club</a></li>
+                            <li><a href="#">Results</a></li>
+                            <li><a href="contact.php">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <hr>
+            <p class="p-3">©Cumilla Shikkha Board Govt. Model College 2020, Designed & Developed by iHealthScreen Bangladesh <a href="https://ihealthscreen.org/">iHealthScreen Bangladesh</a>.</p>
+        </div>
+    </div>
+</footer>
+<!-- Footer Section End -->
+
+<!-- Bootstrap JS CDN -->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="assets/js/page.js"></script>
+<script>
+    const nav = document.querySelector('.navbar');
+    window.addEventListener('scroll', function(){
+        const offset = window.pageYOffset;
+
+        if(offset > 200){
+            nav.classList.add('fixed-top');
+        } else{
+            nav.classList.remove('fixed-top');
+        }
+    });
+</script>
+</body>
+</php>
---
+++ master_file/signin.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="light"><!--begin::Head--><head>
+    <title>
+      Result Processing App
+    </title>
+    <meta charset="utf-8">
+    <meta name="description" content="Result Processing Application">
+    <meta name="keywords" content="Result, Processing, Application">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:locale" content="en_US">
+    <meta property="og:type" content="article">
+    <meta property="og:title" content="Result Processing Application">
+    <meta property="og:url" content="">
+    <meta property="og:site_name" content="">
+    <link rel="canonical" href=""><link rel="shortcut icon" href="images/cumilla-govt-college-logo.jpg">
+    <!--begin::Fonts-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700">
+    <!--end::Fonts-->
+    <link rel="stylesheet" href="assets/css/style.bundle.css">
+    <link rel="stylesheet" href="assets/plugins/global/plugins.bundle.css">
+  <style type="text/css">
+  @font-face {
+    font-weight: 400;
+    font-style:  normal;
+    font-family: circular;
+  
+    src: url('chrome-extension://liecbddmkiiihnedobmlmillhodjkdmb/fonts/CircularXXWeb-Book.woff2') format('woff2');
+  }
+  
+  @font-face {
+    font-weight: 700;
+    font-style:  normal;
+    font-family: circular;
+  
+    src: url('chrome-extension://liecbddmkiiihnedobmlmillhodjkdmb/fonts/CircularXXWeb-Bold.woff2') format('woff2');
+  }</style></head>
+  <!--end::Head-->
+  <!--begin::Body-->
+  
+  <body data-kt-name="metronic" id="kt_body" class="app-blank bgi-size-cover bgi-position-center bgi-no-repeat" data-new-gr-c-s-check-loaded="14.1131.0" data-gr-ext-installed="">
+    <!--begin::Main-->
+    <!--begin::Root-->
+    <div class="d-flex flex-column flex-root">
+      <!--begin::Page bg image-->
+      <style>
+        body {
+          background-image: url("https://www.csbmc.edu.bd/assets/media/auth/bg10.jpeg");
+        }
+  
+        [data-theme="dark"] body {
+          background-image: url("https://www.csbmc.edu.bd/assets/media/auth/bg10-dark.jpeg");
+        }
+      </style>
+      <!--end::Page bg image-->
+      <!--begin::Authentication - Sign-up -->
+      <div class="d-flex flex-column flex-lg-row flex-column-fluid">
+      <div class="d-flex flex-lg-row-fluid">
+          <div class="d-flex flex-column flex-center pb-0 pb-lg-10 p-10 w-100">
+              <img class="theme-light-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="images/agency.png" alt="">
+              <img class="theme-dark-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="https://www.csbmc.edu.bd/assets/media/auth/agency-dark.png" alt="">
+              <h1 class="text-gray-800 fs-2qx fw-bold text-center mb-7">Fast, Efficient and Productive</h1>
+              <div class="text-gray-600 fs-base text-center fw-semibold">In this kind of post,
+                  <a href="#" class="opacity-75-hover text-primary me-1">the blogger</a>introduces a person they’ve interviewed
+                  <br>and provides some background information about
+                  <a href="#" class="opacity-75-hover text-primary me-1">the interviewee</a>and their
+                  <br>work following this is a transcript of the interview.</div>
+          </div>
+      </div>
+      <div class="d-flex flex-column-fluid flex-lg-row-auto justify-content-center justify-content-lg-end p-12">
+          <div class="bg-body d-flex flex-center rounded-4 w-md-600px p-10">
+              <div class="w-md-400px">
+  
+                  <form method="POST" action="" class="form w-100" novalidate="novalidate" id="kt_sign_in_form">
+                      <input type="hidden" name="_token" value="eTM9WBZ7dTIGPy4XDQWWdyYit1rL9phOvkkwaHch">
+                      <div class="text-center mb-11">
+                          <h1 class="text-dark fw-bolder mb-3">Sign In</h1>
+                      </div>
+                      <div class="row g-3 mb-9">
+                          <div class="fv-row mb-8">
+                              <input type="text" placeholder="login" name="login" autocomplete="off" class="form-control bg-transparent" value="">
+                              <br>
+                          </div>
+                          
+                          <div class="fv-row mb-3">
+                              <input type="password" placeholder="Password" name="password" autocomplete="off" class="form-control bg-transparent">
+                              <br>
+                          </div>
+                          
+                          <div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
+                              <div></div>
+                              <a href="resetpassword.html" class="link-primary">Forgot Password ?</a>
+                          </div>
+                          
+                          <div class="d-grid mb-10">
+                              <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
+                                  <span class="indicator-label">Sign In</span>
+                                  <span class="indicator-progress">Please wait...
+                                      <span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
+                              </button>
+                          </div>
+                      </div>
+                  </form>
+              </div>
+          </div>
+      </div>
+  </div>
+      <!--end::Authentication - Sign-up-->
+    </div>
+    <!--end::Root-->
+    <!--end::Main-->
+    <!--begin::Javascript-->
+  
+    <!--begin::Global Javascript Bundle(used by all pages)-->
+    <script src="assets/plugins/global/plugins.bundle.js"></script>
+    <script src="assets/js/scripts.bundle.js"></script>
+    <script>
+      setTimeout(function(){
+          $('.alert').hide();
+          //....and whatever else you need to do
+      }, 3000);
+    </script>
+    <!--end::Custom Javascript-->
+    <!--end::Javascript-->
+  
+  
+  
+  
+  <svg id="SvgjsSvg1001" width="2" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.dev" style="overflow: hidden; top: -100%; left: -100%; position: absolute; opacity: 0;"><defs id="SvgjsDefs1002"></defs><polyline id="SvgjsPolyline1003" points="0,0"></polyline><path id="SvgjsPath1004" d="M0 0 "></path></svg><daily-companion-app></daily-companion-app><div id="loom-companion-mv3" ext-id="liecbddmkiiihnedobmlmillhodjkdmb"><section id="shadow-host-companion"></section></div></body><!--end::Body--><grammarly-desktop-integration data-grammarly-shadow-root="true"></grammarly-desktop-integration>
+  
+  </html>
